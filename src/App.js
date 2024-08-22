193e079939ac68349cb7c6f217be819f8370b00a
1HIM <jlassiayoub800@gmail.com>
2024-08-22 16:43:43 +0100

    't'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+// src/App.js
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
+
+
+// Product data
+const product = {
+  name: "GoPro",
+  price: "$49.99",
+  description: "A cool gadget that makes your life easier by changing your view of photography and documentary.",
+  image: "gopro.jpg"
+};
+
+// Name component
+const Name = () => <h2>{product.name}</h2>;
+
+// Price component
+const Price = () => <p>{product.price}</p>;
+
+// Description component
+const Description = () => <p>{product.description}</p>;
+
+// Image component
+const Image = () => <img src={product.image} alt={product.name} />;
+
+// App component
+const App = () => {
+  const firstName = "Ayoub";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card style={{ width: '18rem' }}>
+        <Image />
+        <Card.Body>
+          <Name />
+          <Price />
+          <Description />
+        </Card.Body>
+      </Card>
+      <div>
+        {firstName ? (
+          <>
+            <p>Hello, {firstName}!</p>
+            <img src="./gopro.jpg" alt={firstName} />
+          </>
+        ) : (
+          <p>Hello, there!</p>
+        )}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
